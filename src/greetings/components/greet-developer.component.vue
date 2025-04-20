310a7c140f1e92c98490d61f26f051dc86f07921
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-19 20:10:29 -0500

    feat(greet-developer): add greet developer component.

---
+++ src/greetings/components/greet-developer.component.vue
@@ -0,0 +1,28 @@
+<script>
+import {Developer} from "../model/developer.entity.js";
+
+export default {
+  name: "greet-developer",
+  props: {
+    firstName: String,
+    lastName: String,
+  },
+  methods: {
+    getGreeting() {
+      if(!this.firstName?.trim() || !this.lastName?.trim()) {
+        return "Welcome Anonymous Developer";
+      }
+      const developer = new Developer(this.firstName, this.lastName);
+      return `Congrats ${developer.fullName}! Now You are a Vue Developer`;
+    }
+  }
+}
+</script>
+
+<template>
+  <p>{{ getGreeting() }}</p>
+</template>
+
+<style scoped>
+
+</style>
